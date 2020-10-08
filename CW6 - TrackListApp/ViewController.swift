e1f7a723e0bf75c40edec0591c1fe339e6d1c57b
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-08 14:06:26 +0300

    Initial Commit

---
+++ CW6 - TrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CW6 - TrackListApp
+//
+//  Created by Сергей Ткаченко on 08.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
