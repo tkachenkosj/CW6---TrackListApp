2842367de056ff4c141370b505e2a9847a971acd
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-08 14:41:17 +0300

    new

--- CW6 - TrackListApp/ViewController.swift
+++ CW6 - TrackListApp/ViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
 
 
