d07a64a4308b7d9d159f1c5a9c0c98932f2c1be0
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-21 15:45:11 +0300

    Разбираюсь с TableView

--- CW6 - TrackListApp/ViewController.swift
+++ CW6 - TrackListApp/ViewController.swift
@@ -7,19 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell index \(indexPath.row)"
         
         return cell
     }
